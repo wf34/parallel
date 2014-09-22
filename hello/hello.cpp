1f0fade3f3cdb1dcf820e1fed1f3a875abec7b72
wf34 <wf34@users.noreply.github.com>
2014-09-22 23:24:24 +0400

    Hello extra bsp_get example with map as storage

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -2,11 +2,50 @@
 #include <stdio.h>
 
 #include <vector>
+#include <map>
 #include <iostream>
 
 extern "C" {
 #include "include/mcbsp.h"
 }
+
+void withMaps() {
+    bsp_begin( 4 );
+    /// Init
+    std::map<char, std::vector<unsigned int> > m;
+    std::vector<unsigned int> a;
+    a.resize(3);
+    m['m'] = a;
+    bsp_push_reg(m['m'].data(), m['m'].capacity()*sizeof(unsigned int));
+
+    if(0 == bsp_pid() % 2) {
+        m['m'] = {bsp_pid()*5+1, bsp_pid()*10+1, bsp_pid()*15+1};
+    }
+
+    bsp_sync();
+    // getting values of even into odd
+    if(1 == bsp_pid() % 2) {
+        bsp_get(bsp_pid() - 1, m['m'].data(), 0,
+                m['m'].data(), 3 * sizeof(unsigned int));
+    }
+    
+    bsp_sync();
+    // print values
+    for(int i = 0; i < 4; ++i) {
+        if(i == bsp_pid()) {
+            std::cout << "Proc {" << bsp_pid() << "} contains"
+                      << std::endl << std::flush;
+            for(std::vector<unsigned int>::const_iterator it = m['m'].cbegin();
+                it != m['m'].cend(); ++it)
+                std::cout << *it << " "; 
+            std::cout << std::endl << std::flush;
+        }
+        bsp_sync();
+    }
+    bsp_pop_reg(m['m'].data());
+    bsp_end();
+}
+
 
 void spmd() {
     bsp_begin( 4 );
@@ -44,7 +83,7 @@ void spmd() {
 }
 
 int main( int argc, char ** argv ) {
-    bsp_init( &spmd, argc, argv );
+    bsp_init( &withMaps, argc, argv );
     spmd();
     return EXIT_SUCCESS;
 }
