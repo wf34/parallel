d886007dfe2fdc646f47036f3731fa9f82ae62a2
wf34 <wf34@users.noreply.github.com>
2014-09-22 17:09:03 +0400

    helloworld rewritten to demonstate simple bsp get

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,24 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+#include <vector>
+#include <iostream>
+
 extern "C" {
 #include "include/mcbsp.h"
 }
-unsigned int P;
 
 void spmd() {
-    bsp_begin( P );
-    printf( "Hello world from thread %d out of %d!\n", bsp_pid(), bsp_nprocs() );
+    bsp_begin( 4 );
+    /// Init
+    std::vector<unsigned int> a;
+    a.resize(3);
+    bsp_push_reg(a.data(), a.capacity()*sizeof(unsigned int));
+
+    if(0 == bsp_pid() % 2) {
+        a = {bsp_pid()*5+1, bsp_pid()*10+1, bsp_pid()*15+1};
+    }
+
+    bsp_sync();
+    // getting values of even into odd
+    if(1 == bsp_pid() % 2) {
+        bsp_get(bsp_pid()-1, a.data(), 0,
+                a.data(), 3*sizeof(unsigned int));
+    }
+    
+    bsp_sync();
+    // print values
+    for(int i = 0; i < 4; ++i) {
+        if(i == bsp_pid()) {
+            std::cout << "Proc {" << bsp_pid() << "} contains"
+                      << std::endl << std::flush;
+            for(std::vector<unsigned int>::const_iterator it = a.cbegin();
+                it != a.cend(); ++it)
+                std::cout << *it << " "; 
+            std::cout << std::endl << std::flush;
+        }
+        bsp_sync();
+    }
+    bsp_pop_reg(a.data());
     bsp_end();
 }
 
 int main( int argc, char ** argv ) {
-    printf( "How many threads do you want started? There are %d cores available.\n", bsp_nprocs() );
-    fflush( stdout );
-    scanf( "%d", &P );
-    if( P == 0 || P > bsp_nprocs() ) {
-        fprintf( stderr, "Cannot start %d threads.\n", P );
-        return EXIT_FAILURE;
-    }
     bsp_init( &spmd, argc, argv );
     spmd();
     return EXIT_SUCCESS;
