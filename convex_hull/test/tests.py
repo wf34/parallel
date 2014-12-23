35bf709ee7dfb76341d6f6f41ab3d34c3ce07b5d
dmitri kovalenko <kovalenko.d@partner.samsung.com>
2014-12-23 20:36:16 +0300

    [ConvexHull]: GrahamScan in progress

--- convex_hull/test/tests.py
+++ convex_hull/test/tests.py
@@ -1,15 +1,28 @@
 import subprocess
 from subprocess import Popen, PIPE, STDOUT
 
-def test ():
-    rbox_process = Popen (['rbox', '100', 'D2'], stdout = PIPE)
-    problem = rbox_process.communicate ()
-    print (problem[0])
-    
-    qh = subprocess.Popen(['qhull', 'p'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+convex_hull_app = "C:/projects/parallel/convex_hull/build/Release/convex_hull.exe"
 
+
+def test ():
+    rbox_process = Popen (['rbox', '10', 'D2'], stdout = PIPE)
+    problem = rbox_process.communicate ()
+    print "==========\n", problem[0], "==========\n"
+
+    qh = subprocess.Popen(['qhull', 'p'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     qhull_output = qh.communicate (input=problem[0])[0]
-    print ("Qhull Result:\n", qhull_output)
+    print "Qhull Result:\n", qhull_output
+
+    ch = subprocess.Popen([convex_hull_app, 'p'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    convex_hull_output = ch.communicate (input=problem[0])[0]
+    print "Our Result:\n", convex_hull_output
+
+    if (qhull_output == convex_hull_output):
+        print "test passed"
+    else:
+        print "test failed"
+
+
 
 if __name__ == "__main__":
     test ()
