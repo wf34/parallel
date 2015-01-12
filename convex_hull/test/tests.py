2d1b43357a37061e616679b5a76f9e30b3e08c43
wf34 <wf34@users.noreply.github.com>
2015-01-13 00:35:51 +0300

    [ConvexHull] tester script edits

--- convex_hull/test/tests.py
+++ convex_hull/test/tests.py
@@ -1,7 +1,11 @@
+import os
 import subprocess
 from subprocess import Popen, PIPE, STDOUT
 
-convex_hull_app_ = "C:/projects/parallel/convex_hull/build/Release/convex_hull.exe"
+convex_hull_app_ = \
+    ("C:/projects/parallel/convex_hull/build/Release/convex_hull.exe" \
+    if (os.name == "nt") else \
+        "/home/wf34/projects/parallel/convex_hull/build/convex_hull")
 allowed_error_ = float (1e-9)
 
 
@@ -53,29 +57,36 @@ def hull_test (seed):
         print "test failed"
 
 
+def is_enet_valid (set, enet):
+    center = []
+    return False
+
+
 
 def check_enet (seed):
     seeding_argument = 't' + str(seed)
-    rbox_process = Popen (['rbox', '64', 'D2', seeding_argument], stdout = PIPE, shell = True)
-    problem = rbox_process.communicate ()
-    rbox_output = problem[0]
-    # print "==========\n", problem[0], "==========\n"
+    rbox_process = Popen (['rbox', '64', 'D2', seeding_argument], stdout = PIPE)
+    rbox_output = rbox_process.communicate ()[0]
+    # print "==========\n", rbox_output, "==========\n"
 
-    ch = subprocess.Popen([convex_hull_app_, 'p'], stdout=PIPE, stdin=PIPE, stderr=STDOUT, shell = True)
-    convex_hull_output = ch.communicate (input=problem[0])[0]
+    ch = subprocess.Popen([convex_hull_app_, 'p'], stdout=PIPE, stdin=PIPE)
+    convex_hull_output = ch.communicate (input=rbox_output)[0]
     # print "Our Result:\n", convex_hull_output, "==========\n"
-
     enet = deserialize_cycle (convex_hull_output)
+
     first_line_end = rbox_output.find ('\n')
     rbox_data = rbox_output [first_line_end + 1:]
-    #print "==========\n", rbox_data, "==========\n"
+    # print "==========\n", rbox_data, "==========\n"
     whole_pointset = deserialize_cycle (rbox_data)
     # print "whole_pointset ==========="
     # for x in range (len (whole_pointset)):
     #     print whole_pointset[x][0], " ", whole_pointset[x][1]
-    print "enet ==========="
-    for x in range (len (enet)):
-        print enet[x][0], " ", enet[x][1]
+    # print "enet ==========="
+    # for x in range (len (enet)):
+    #     print enet[x][0], " ", enet[x][1]
+    if not is_enet_valid (whole_pointset, enet):
+        print "e-net test failed"
+
 
 
 def testing ():
