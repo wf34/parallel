7b89754593f679678a1f0fa09dcb7b842f380725
wf34 <wf34@users.noreply.github.com>
2015-01-18 03:07:00 +0300

    [ConvexHull] graph improv

--- docs/reports/2015.01.14/pretty_graph.R
+++ docs/reports/2015.01.14/pretty_graph.R
@@ -2,5 +2,5 @@ library (ggplot2)
 library (reshape2)
 
 chTimes = read.csv("ch_times.csv")
-ggplot(melt (chTimes, id.var = "problemSize"), aes(x = problemSize, y = value, colour = variable)) + geom_line() + xlab("Problem Size") + ylab("Time")
-
+plot = ggplot(melt (chTimes, id.var = "problemSize"), aes(x = problemSize, y = value, colour = variable)) + geom_line() + xlab("Problem Size") + ylab("Time")
+ggsave ("ch_times.png", plot)
