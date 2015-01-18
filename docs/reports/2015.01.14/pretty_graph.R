6ea9e3a468dab9448819e0f63bf1522f67231063
wf34 <wf34@users.noreply.github.com>
2015-01-19 00:08:59 +0300

    [ConvexHull] report finished

--- docs/reports/2015.01.14/pretty_graph.R
+++ docs/reports/2015.01.14/pretty_graph.R
@@ -1,6 +1,7 @@
 library (ggplot2)
 library (reshape2)
+library(scales)
 
 chTimes = read.csv("ch_times.csv")
-plot = ggplot(melt (chTimes, id.var = "problemSize"), aes(x = problemSize, y = value, colour = variable)) + geom_line() + xlab("Problem Size") + ylab("Time")
+plot = ggplot(melt (chTimes, id.var = "problemSize"), aes(x = problemSize, y = value, colour = variable)) + geom_line() + geom_point ()+ xlab("Problem Size") + ylab("Time") + scale_x_continuous(labels = comma)
 ggsave ("ch_times.png", plot)
